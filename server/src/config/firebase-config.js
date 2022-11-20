b08733d32990d432b0c51e9b18046b23714316a5
tarquibrian <tarquibrian@gmail.com>
2022-11-19 21:03:44 -0400

    api data

--- server/src/config/firebase-config.js
+++ server/src/config/firebase-config.js
@@ -1,14 +1,14 @@
 require("dotenv").config();
 
-const admin = require('firebase-admin')
+const admin = require("firebase-admin");
 const { initializeApp, applicationDefault } = require("firebase-admin/app");
 const { getFirestore } = require("firebase-admin/firestore");
 const { getAuth } = require("firebase-admin/auth");
 
-const serviceAccount = require('../../firebase.json');
+const serviceAccount = require("../../firebase.json");
 
 initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert(serviceAccount),
 });
 
 const db = getFirestore();
--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,58 +1,97 @@
 const { Router } = require("express");
-const { db } = require("../config/firebase-config");
+const { db, auth } = require("../config/firebase-config");
 const path = require("path");
 const { QuerySnapshot } = require("firebase-admin/firestore");
 const router = Router();
 
 router.get("/users", async (req, res) => {
   try {
-    const result = await db.collection("users").get();
+    const result = await db
+      .collection("users")
+      .doc("xAyFZciemwTp1QZWQq3I")
+      .collection("suscription")
+      .get();
 
     const docs = result.docs.map((doc) => ({
-      id: doc.id,
+      // id: doc.id,
       ...doc.data(),
     }));
 
-    console.log(docs);
+    // console.log(docs);
 
+    // res.send(docs);
+
+    // result.forEach((doc) => {
+    //   console.log(doc.id, '>', doc.data())
+    // });
     res.send(docs);
   } catch (error) {
     console.log(error);
   }
 });
 
-router.post("/new-user", (req, res) => {
-  const { firstname, lastname, email, password } = req.body;
+router.post("/all", async (req, res) => {
+  const result = await await auth.listUsers();
 
-  console.log(firstname, lastname, email, password);
+  res.send(result);
+});
 
-  res.send("new user created");
+router.post("/new-user", async (req, res) => {
+  const { email, password } = req.body;
+
+  const result = await auth.createUser({
+    email: email,
+    passworda: password,
+    emailVerified: false,
+    disabled: false
+  });
+
+  res.send(result);
 });
 
 router.post("/new-suscription", async (req, res) => {
+  const { iduser } = req.body;
 
-  let id = {}
+  const months = {
+    enero: false,
+    febrero: false,
+    marzo: false,
+    abril: false,
+    mayo: false,
+    junio: false,
+    julio: false,
+    agosto: false,
+    septiembre: false,
+    octubre: false,
+    noviembre: false,
+    diciembre: false,
+  };
 
   const result = await db
-    .collection("suscriptions")
-    .where("id_user", "==", "G52hPFALu5dygtTpSmJz");
-
-  result.get().then((querySnapshot) => {
-    querySnapshot.forEach((documentSnapshot) => {
-      let data = documentSnapshot.data()
-      return res.send((data));
+    .collection("users")
+    .doc(iduser)
+    .collection("suscription")
+    .doc()
+    .set({
+      ...months,
     });
-  });
 
-  // const doc = result.get().then((querySnapshot) => {
-  //   // console.log(querySnapshot);
-  //   querySnapshot.forEach((documentSnapshot) => {
-  //     // console.log(`Found document at ${documentSnapshot.ref.path}`);
-  //     item: documentSnapshot.data();
-  //   });
-  // });
+  res.send(result);
+});
 
-  // res.send(id);
+router.put("/update-suscription", async (req, res) => {
+  const { iduser, months } = req.body;
+
+  await db
+    .collection("users")
+    .doc(iduser)
+    .collection("suscription")
+    .doc("vrnK4My9ZR6Qa1Wx85R4")
+    .update({
+      ...months,
+    });
+
+  res.send({ iduser, months });
 });
 
 module.exports = router;
